640fff4bb25278210ead8bd6ef252ad2110d7f2a
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-04-12 20:40:06 +0900

    Fix: import 에러 수정

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,38 +1,38 @@
-import { allPosts } from '@/.contentlayer/generated'
-import { MDXComponents } from 'mdx/types'
-import { useMDXComponent } from 'next-contentlayer/hooks'
-import { notFound } from 'next/navigation'
+import { allPosts } from 'contentlayer/generated';
+import { MDXComponents } from 'mdx/types';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+import { notFound } from 'next/navigation';
 
 const component: MDXComponents = {
   a: ({ href, children }) => (
-    <a href={href as string} className="text-red">
+    <a href={href as string} className='text-red'>
       {children}
     </a>
   ),
-}
+};
 
 const PostPage = ({ params }: { params: { slug: string[] } }) => {
-  const slug = decodeURI(params.slug.join('/'))
+  const slug = decodeURI(params.slug.join('/'));
 
-  const post = allPosts.find((p) => p.slug === slug)
-  const postContent = post?.body.code
+  const post = allPosts.find((p) => p.slug === slug);
+  const postContent = post?.body.code;
 
   if (!postContent) {
-    return notFound()
+    return notFound();
   }
 
-  const MDXComponent = useMDXComponent(postContent)
+  const MDXComponent = useMDXComponent(postContent);
   return (
-    <article className="max-w-full p-4 prose mt-14 prose-gray dark:prose-invert prose-code">
+    <article className='max-w-full p-4 prose mt-14 prose-gray dark:prose-invert prose-code'>
       <div>
-        <div className="mb-3 text-4xl font-black text-center">{post.title}</div>
-        <div className="mb-16 text-right">{post.date.slice(0, 10)}</div>
+        <div className='mb-3 text-4xl font-black text-center'>{post.title}</div>
+        <div className='mb-16 text-right'>{post.date.slice(0, 10)}</div>
       </div>
       <div>
         <MDXComponent component={component} />
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
