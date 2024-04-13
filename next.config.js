178c96e1f42c73f68ed6c6da873f317752952bd6
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-04-13 15:26:46 +0900

    Fix: Github Pages 배포 오류

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require('next-contentlayer2');
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer({
   reactStrictMode: true,
+  output: 'export',
 });
 
 module.exports = nextConfig;
