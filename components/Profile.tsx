f91317f10768f3173e45f8c0ac85cb1874f4f26a
Doyeon Kim <doyeonkim.yeon@gmail.com>
2024-04-17 15:40:27 +0900

    Feat: Profile 문구 변경

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,16 +1,22 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 const Profile = () => {
   return (
-    <div className="grid grid-cols-5 gap-4 p-4 md:grid-cols-6">
-      <div className="relative flex items-center justify-center col-span-2 rounded-lg text-9xl aspect-square bg-gray-100/10">
-        <Image src="/astronaut.png" alt="astronaut" fill className="transform scale-90" />
+    <div className='grid grid-cols-5 gap-4 p-4 md:grid-cols-6'>
+      <div className='relative flex items-center justify-center col-span-2 rounded-lg text-9xl aspect-square bg-gray-100/10'>
+        <Image
+          src='/astronaut.png'
+          alt='astronaut'
+          fill
+          className='transform scale-90'
+        />
       </div>
-      <div className="col-span-3 md:col-span-4">
-        <h1 className="text-2xl font-bold">안녕하세요 💫</h1>
+      <div className='col-span-3 md:col-span-4'>
+        {/* <h1 className='text-2xl font-bold'>안녕하세요 💫</h1> */}
+        <h1 className='text-2xl font-bold'>블로그 공사중 👷‍♂️</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
